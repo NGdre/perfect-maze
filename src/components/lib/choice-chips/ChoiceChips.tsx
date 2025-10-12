610cfed8113fa55c908e6ea7c5c28e2ae25190eb
NGdre <100stepstosuccess@gmail.com>
2025-10-13 00:20:22 +0300

    [STYLE]: tweak design of ChoiceChips

--- src/components/lib/choice-chips/ChoiceChips.tsx
+++ src/components/lib/choice-chips/ChoiceChips.tsx
@@ -111,8 +111,8 @@ export const ChoiceChips: React.FC<ChoiceChipsProps> = ({
           <button
             key={option.value}
             className={clsx(
-              "inline-flex min-h-8 cursor-pointer items-center gap-1.5 rounded-full border border-gray-200 bg-gray-50 px-4 py-1.5 capitalize outline-none transition-all duration-200 hover:bg-gray-100 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2",
-              isSelected && "!border-primary-500 text-primary-700 bg-blue-50",
+              "inline-flex min-h-8 cursor-pointer items-center gap-1.5 rounded-full border border-gray-200 bg-gray-50 px-4 py-1.5 capitalize outline-none transition-all duration-200 hover:bg-primary-500 hover:text-white focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2",
+              isSelected && "!border-primary-500 bg-blue-50 text-primary-700",
               option.disabled && "cursor-not-allowed opacity-60",
               chipClassName,
             )}
