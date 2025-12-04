095991bb0b9faa8011879ea1db2942586dbfa968
NGdre <100stepstosuccess@gmail.com>
2025-12-05 02:29:48 +0300

    [FIX]: fix unused React import
    React import had to be declared in some tests that uses jsx, even if it is unused
    
    "react-jsx" option transform automatically imports the necessary functions for JSX compilation, eliminating the need to explicitly import React at the top of every file that uses JSX.

--- src/components/lib/choice-chips/ChoiceChips.test.tsx
+++ src/components/lib/choice-chips/ChoiceChips.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import React from "react";
-
 import { ChipOption, ChoiceChips } from "./ChoiceChips";
 
 const MockIcon = () => <span data-testid="mock-icon"></span>;
