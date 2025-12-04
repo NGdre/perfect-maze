243e44ab6db1e8728b5353110fc42c9b5180ce86
NGdre <100stepstosuccess@gmail.com>
2025-12-04 22:31:52 +0300

    [FEAT]: migrate deployment to Vercel from GitHub Pages
    - update vite base config for production
    - remove GH Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: process.env.NODE_ENV === "production" ? "/perfect-maze/" : "./",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
